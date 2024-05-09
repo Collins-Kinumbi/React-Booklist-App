0ae9c623e4c5cf3b486aee58d130278ceea37ab7
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-05-09 11:43:38 +0300

    Implementing localStorage

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,20 +1,27 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 // import { v4 as uuidv4 } from "uuid";
 import BookReducer from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 function BookContextProvider({ children }) {
-  const [books, dispatch] = useReducer(BookReducer, [
-    // { title: "Fullmetal Alchemist", author: "Hiromu Arakawa", id: 1 },
-    // { title: "Fire Punch", author: "Tatsuki Fijimoto", id: 2 },
-    // { title: "Ranger Reject", author: "Negi Haruba", id: 3 },
-    // {
-    //   title: "Frieren: Beyond Journeys' end",
-    //   author: "Kanehito Yamada",
-    //   id: 4,
-    // },
-  ]);
+  const [books, dispatch] = useReducer(
+    BookReducer,
+    [
+      // { title: "Fullmetal Alchemist", author: "Hiromu Arakawa", id: 1 },
+      // { title: "Fire Punch", author: "Tatsuki Fijimoto", id: 2 },
+      // { title: "Ranger Reject", author: "Negi Haruba", id: 3 },
+      // {
+      //   title: "Frieren: Beyond Journeys' end",
+      //   author: "Kanehito Yamada",
+      //   id: 4,
+      // },
+    ],
+    () => {
+      const localData = localStorage.getItem("Books");
+      return localData ? JSON.parse(localData) : [];
+    }
+  );
 
   // function addBook(title, author) {
   //   setBooks([...books, { title: title, author: author, id: uuidv4() }]);
@@ -27,6 +34,10 @@ function BookContextProvider({ children }) {
   //     })
   //   );
   // }
+
+  useEffect(() => {
+    localStorage.setItem("Books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
